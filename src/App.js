a0109acf136326faec2517df84432a7b47c777d0
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-08-16 00:38:44 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select>
+        <option value="meme1">img de meme 2</option>
+        <option value="meme2">img de meme 1</option>
+        <option value="meme3">img de meme 3</option>
+        <option value="meme4">img de meme 4</option>
+        <option value="meme5">img de meme 5</option>
+        <option value="meme6">img de meme 6</option>
+      </select>
+      <br/>
+
+      <input type="text" placeholder="linea 1"></input><br />
+      <input type="text" placeholder="linea 2"></input><br/>
+      <button>Exportar</button>
+      <div>
+        <spa>Linea 1</spa>
+        <spa>Linea 2</spa>
+      </div>
     </div>
   );
 }
