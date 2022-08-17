fc930cad1e6c44b5c9c3e0047b8ae80940da2600
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-08-17 00:38:58 -0500

    se agregaron las imagenes a usar como templates

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [linea1, setLinea1] = useState("hola mundo");
-  const [linea2, setLinea2] = useState("hola mundo");
+  const [linea1, setLinea1] = useState("");
+  const [linea2, setLinea2] = useState("");
+  const [imagen, setImagen] = useState("");
 
   const onChangeLinea1 = e => setLinea1(e.target.value);
   const onChangeLinea2 = e => setLinea2(e.target.value);
+  const onChangeImagen = e => setImagen(e.target.value);
 
   return (
     <div className="App">
-      <select>
+      <select onChange={onChangeImagen}>
         <option value="meme1">img de meme 2</option>
         <option value="meme2">img de meme 1</option>
         <option value="meme3">img de meme 3</option>
@@ -25,12 +27,15 @@ function App() {
         type="text"
         placeholder="linea 1">
       </input><br />
+
       <input
         onChange={onChangeLinea2}
         type="text"
         placeholder="linea 2">
       </input><br />
+
       <button>Exportar</button>
+
       <div>
         <span>{linea1}</span><br />
         <span>{linea2}</span>
